c4aaac7a332a556011da605142eda3806eae2efd
faringen <melamurlerina@gmail.com>
2026-02-19 09:26:34 +0300

    Add a Practic 3 to Labaratory_3.java file

--- src/Labaratory_3/Labaratory_3.java
+++ src/Labaratory_3/Labaratory_3.java
@@ -1,5 +1,30 @@
 package Labaratory_3;
 
-class Labaratory_3{
+import java.util.Arrays;
+import java.util.Scanner;
 
+class Labaratory_3 {
+    static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите количество элементов:");
+        int count = in.nextInt();
+        int[] sortArr = new int[count];
+        for(int i = 0; i<count; i++){
+            sortArr[i] = in.nextInt();
+        }
+        insertionSort(sortArr);
+        System.out.println(Arrays.toString(sortArr));
+    }
+    public static void insertionSort(int[] sortArr) {
+        int j;
+        for (int i = 1; i < sortArr.length; i++) {
+            if(sortArr[i] % 2 ==0){
+                int swap = sortArr[i];
+                for (j = i; j > 0 && swap < sortArr[j - 1]; j--) {
+                    sortArr[j] = sortArr[j - 1];
+                }
+                sortArr[j] = swap;
+            }
+        }
+    }
 }
