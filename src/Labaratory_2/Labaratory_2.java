cec3b2ed5d9eab929fb8c14b55c8e0cba868fc70
faringen <melamurlerina@gmail.com>
2026-02-19 09:29:06 +0300

    Add a Practic 2 in file Labaratory_2.java

--- src/Labaratory_2/Labaratory_2.java
+++ src/Labaratory_2/Labaratory_2.java
@@ -1,5 +1,27 @@
 package Labaratory_2;
 
-class Labaratory_2{
+import java.util.Scanner;
 
+class Labaratory_2{
+    static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Введите количество повторений:");
+        double count = input.nextDouble();
+        System.out.println("Введите значение x(<1):");
+        double x = input.nextDouble();
+        System.out.println("arccos = " + formula(count, x));
+        System.out.println(Math.acos(x));
+
+    }
+    static double formula(double i, double x){
+        double res = x;
+        if(Math.abs(x)<1){
+            for (int n = 1; n <= i; n++) {
+                double up = (2*n - 1) * Math.pow(x, 2 * n + 1);
+                double down = 2 * n *(2*n + 1);
+                res += up/down;
+            }
+        }
+        return (Math.PI/2)-res;
+    }
 }
