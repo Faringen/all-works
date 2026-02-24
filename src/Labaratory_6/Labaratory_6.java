e13f11fd2f6017156d8815038ee5ae3b286cc825
faringen <melamurlerina@gmail.com>
2026-02-24 23:26:01 +0300

    Edit a class Reader

--- src/Labaratory_6/Labaratory_6.java
+++ src/Labaratory_6/Labaratory_6.java
@@ -1,7 +1,11 @@
 package Labaratory_6;
 
 class Labaratory_6{
+    static void main(){
+        Reader libery = new Reader("Сидоров В.А.", "312", "4 курс","25.12.2001", "8992543151");
+        libery.takeBook(3);
 
+    }
 }
 class Reader{
     String name, number, facultet,data, phoneNumber;
@@ -15,7 +19,21 @@ class Reader{
     void takeBook(int countBook){
         System.out.printf("%s взял %d книги.", name, countBook);
     }
+    void takeBook(String... book){
+        System.out.printf("%s взял книги: ");
+        for (int i = 0; i < book.length; i++) {
+            System.out.println(book[i]);
+            if(i < book.length-1) System.out.println(" , ");
+        }
+    }
     void returnBook(int countBook){
         System.out.printf("%s вернул %d книги.", name, countBook);
+    }
+    void returnBook(String... book){
+        System.out.printf("%s отдал книги: ");
+        for (int i = 0; i < book.length; i++) {
+            System.out.println(book[i]);
+            if(i < book.length-1) System.out.println(" , ");
+        }
     }
 }
