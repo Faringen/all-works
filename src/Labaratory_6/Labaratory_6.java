9423db9bab3e5907bf99d5ae2deaa4d6f6822112
faringen <melamurlerina@gmail.com>
2026-02-24 23:09:23 +0300

    Create a class Reader

--- src/Labaratory_6/Labaratory_6.java
+++ src/Labaratory_6/Labaratory_6.java
@@ -3,3 +3,19 @@ package Labaratory_6;
 class Labaratory_6{
 
 }
+class Reader{
+    String name, number, facultet,data, phoneNumber;
+    Reader(String name, String number, String facultet, String data, String phoneNumber){
+        this.name =name;
+        this.data = data;
+        this.number = number;
+        this.facultet = facultet;
+        this.phoneNumber = phoneNumber;
+    }
+    void takeBook(int countBook){
+        System.out.printf("%s взял %d книги.", name, countBook);
+    }
+    void returnBook(int countBook){
+        System.out.printf("%s вернул %d книги.", name, countBook);
+    }
+}
