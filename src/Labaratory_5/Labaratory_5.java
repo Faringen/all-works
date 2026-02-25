6602c5ef81f0de3c16e29dff02e5331064823a2f
faringen <melamurlerina@gmail.com>
2026-02-25 08:36:48 +0300

    Add a testing numbers int Labaratory_5

--- src/Labaratory_5/Labaratory_5.java
+++ src/Labaratory_5/Labaratory_5.java
@@ -1,11 +1,17 @@
 package Labaratory_5;
 
 class Labaratory_5{
-
+    static void main(){
+        Equation newPrimer = new Equation(5,1,2);
+        int one = newPrimer.first();
+        int two = newPrimer.second();
+        int three = newPrimer.thirt();
+        System.out.println(one + " " + two + " " + three);
+    }
 }
 class Equation{
     int x, a, b;
-    Equation(){
+    Equation(int x, int a, int b){
         this.a = a;
         this.x = x;
         this.b = b;
