2c3baa6695738aba8d4ec0ff847e2caabf43e8d1
faringen <melamurlerina@gmail.com>
2026-02-25 08:26:50 +0300

    Create new class Equation in Labaratory_5

--- src/Labaratory_5/Labaratory_5.java
+++ src/Labaratory_5/Labaratory_5.java
@@ -3,4 +3,28 @@ package Labaratory_5;
 class Labaratory_5{
 
 }
+class Equation{
+    int x, a, b;
+    Equation(){
+        this.a = a;
+        this.x = x;
+        this.b = b;
+    }
+    int first(){
+        return 3*x+5;
+    }
+    int second(){
+        return (a + b)/(a-b);
+    }
+    int thirt(){
+        return getFactorial((a*x)/b);
+    }
+    public static int getFactorial(int f) {
+        int result = 1;
+        for (int i = 1; i <= f; i++) {
+            result = result * i;
+        }
+        return result;
+    }
 
+}
